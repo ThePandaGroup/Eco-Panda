661052b5c50819d68d238cfa6379e72c37eaf2d7
Alexchen2001 <alexchen9899627@gmail.com>
2024-02-03 13:46:39 -0800

    Added customizedcardwidget with background in profile page

--- eco_panda/lib/eprofile.dart
+++ eco_panda/lib/eprofile.dart
@@ -13,21 +13,24 @@ class EProfile extends StatefulWidget {
 class _EProfileState extends State<EProfile> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
+    return const SingleChildScrollView(
         child: Padding(
-        padding: const EdgeInsets.all(12.0),
+        padding: EdgeInsets.all(12.0),
     child: Column(
-      children: [profileSection()],
+      children: [ProfileSection()],
     )
     )
     );
   }
 }
 
-class profileSection extends StatelessWidget{
+class ProfileSection extends StatelessWidget{
+  const ProfileSection({super.key});
+
   @override
   Widget build(BuildContext context){
-    return CustomContainerCard(
+    return CustomContainerCardWithBackground(
+      backgroundImage: "assets/environmental_background.png",
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -78,5 +81,63 @@ class profileSection extends StatelessWidget{
       ),
     );
   }
+}
 
+/*
+*Description: A card can contain background image
+* notes: It is an inheritance from customContainer Card, it is a child class
+* param: child widgets, background image
+ */
+class CustomContainerCardWithBackground extends CustomContainerCard {
+  final String backgroundImage;
+
+  const CustomContainerCardWithBackground({
+    Key? key,
+    required Widget child,
+    EdgeInsets margin = const EdgeInsets.symmetric(vertical: 10.0),
+    EdgeInsets padding = const EdgeInsets.all(9.0),
+    Color borderColor = const Color(0xFFF5FBE5),
+    double borderWidth = 2.0,
+    double borderRadius = 4.0,
+    required this.backgroundImage,
+  }) : super(
+    key: key,
+    child: child,
+    margin: margin,
+    padding: padding,
+    borderColor: borderColor,
+    borderWidth: borderWidth,
+    borderRadius: borderRadius,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    // Use the same structure but add a BoxDecoration with an image
+    return Container(
+      width: double.infinity,
+      margin: margin,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: borderColor,
+          width: borderWidth,
+        ),
+        borderRadius: BorderRadius.circular(borderRadius),
+        image: DecorationImage(
+          image: AssetImage(backgroundImage),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Card(
+        margin: EdgeInsets.zero,
+        color: Colors.transparent, // Make card background transparent
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(borderRadius),
+        ),
+        child: Padding(
+          padding: padding,
+          child: child,
+        ),
+      ),
+    );
+  }
 }
