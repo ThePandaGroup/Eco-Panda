a82e53dce41a9536e8408e32dbbfa23bed38b53b
Sergio Satyabrata <sergio.satyabrata@gmail.com>
2024-02-05 21:42:59 -0800

    edited challenges_test

--- eco_panda/test/challenges_test.dart
+++ eco_panda/test/challenges_test.dart
@@ -1,21 +1,55 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:eco_panda/main.dart';
+// import 'package:eco_panda/echallenges.dart';
+// void main() {
+//   testWidgets('Navigate to Challenges Page and Verify Content', (WidgetTester tester) async {
+//     await tester.pumpWidget(MyApp());
+//
+//     await tester.tap(find.byIcon(Icons.emoji_events));
+//     await tester.pumpAndSettle(); // Wait for the page transition.
+//
+//     // Verify the Challenges page is displayed.
+//     expect(find.byType(EChallenges), findsOneWidget);
+//
+//
+//     //  checking for a specific challenge's presence.
+//     expect(find.text('Daily Challenge'), findsWidgets);
+//     expect(find.text('First Eco route of the day'), findsWidgets);
+//
+//   });
+// }
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:eco_panda/main.dart';
 import 'package:eco_panda/echallenges.dart';
+
 void main() {
   testWidgets('Navigate to Challenges Page and Verify Content', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
 
     await tester.tap(find.byIcon(Icons.emoji_events));
-    await tester.pumpAndSettle(); // Wait for the page transition.
+    await tester.pumpAndSettle();
 
-    // Verify the Challenges page is displayed.
+
     expect(find.byType(EChallenges), findsOneWidget);
 
 
-    //  checking for a specific challenge's presence.
-    expect(find.text('Daily Challenge'), findsWidgets);
-    expect(find.text('First Eco route of the day'), findsWidgets);
+    expect(find.text('Daily Challenge'), findsOneWidget);
+    expect(find.text('First Eco route of the day'), findsOneWidget);
+    expect(find.byType(ChallengeProgressIndicator), findsWidgets);
 
+    expect(find.text('New User Challenge'), findsOneWidget);
+    expect(find.text('Plan your first route'), findsOneWidget);
+    expect(find.byType(ChallengeProgressIndicator), findsWidgets);
+
+    expect(find.text('Weekly Challenge'), findsOneWidget);
+    expect(find.text('I planned 10 rountes in the app !'), findsOneWidget);
+    expect(find.byType(ChallengeProgressIndicator), findsWidgets);
+
+
+    expect(find.text('0/10'), findsOneWidget); // Weekly Challenge progress
   });
 }
