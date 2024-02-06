5b914b7d932e1021b302e1f3e29341723a6ce10f
Sergio Satyabrata <sergio.satyabrata@gmail.com>
2024-02-05 21:53:40 -0800

    edited settings_test

--- eco_panda/test/settings_test.dart
+++ eco_panda/test/settings_test.dart
@@ -1,9 +1,38 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:eco_panda/page_template.dart';
+//
+// void main() {
+//   testWidgets('Navigate to Settings page', (WidgetTester tester) async {
+//     await tester.pumpWidget(MaterialApp(home: EPageTemplate()));
+//
+//     // Tap the bottom navigation bar item for Profile
+//     await tester.tap(find.byIcon(Icons.person));
+//     await tester.pumpAndSettle();
+//
+//     // Tap on the "Edit Profile" button
+//     await tester.tap(find.text('Edit Profile'));
+//     await tester.pumpAndSettle();
+//
+//     // Verify settings page is displayed by checking for specific widgets
+//     expect(find.text('Profile Setting'), findsOneWidget);
+//     expect(find.text('Notifications'), findsOneWidget);
+//     expect(find.text('Permission Access'), findsOneWidget);
+//
+//     expect(find.byType(Switch), findsWidgets);
+//   });
+// }
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:eco_panda/main.dart';
+import 'package:eco_panda/esettings.dart'; // Import your ESettings class
+
 import 'package:eco_panda/page_template.dart';
 
 void main() {
-  testWidgets('Navigate to Settings page', (WidgetTester tester) async {
+  testWidgets('Navigate to Settings Page and Verify Content', (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(home: EPageTemplate()));
 
     // Tap the bottom navigation bar item for Profile
@@ -14,14 +43,29 @@ void main() {
     await tester.tap(find.text('Edit Profile'));
     await tester.pumpAndSettle();
 
-    // Verify settings page is displayed by checking for specific widgets
+    // Verify the presence of specific components on the Settings page.
     expect(find.text('Profile Setting'), findsOneWidget);
     expect(find.text('Notifications'), findsOneWidget);
     expect(find.text('Permission Access'), findsOneWidget);
 
-    expect(find.byType(Switch), findsWidgets);
+
+    // Verify the profile name setting section.
+    expect(find.text('Current Name: John Hougland'), findsOneWidget);
+    expect(find.byType(TextField), findsOneWidget);
+
+
+    // Verify the notification settings section.
+    expect(find.text('Pop-up Notification'), findsOneWidget);
+    expect(find.text('Email Notification'), findsOneWidget);
+
+
+    // Verify the access privilege settings section.
+    expect(find.text('Camera'), findsOneWidget);
+    expect(find.text('GPS'), findsOneWidget);
+    expect(find.text('Contacts'), findsOneWidget);
+    expect(find.text('Other Features'), findsOneWidget);
+
   });
 }
 
 
-// Test
